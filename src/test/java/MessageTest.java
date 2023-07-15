9cdce8d0de04a5d5bf351364a4e5f1aa964ee3be
webmagiconline1 <78255428+webmagiconline1@users.noreply.github.com>
2023-07-15 15:08:41 +0530

    Create MessageTest.java

---
+++ src/test/java/MessageTest.java
@@ -0,0 +1,17 @@
+package com.example.web.models;
+
+public class Message {
+    private String text;
+
+    public Message(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
